cd9d3b9b16080f724ddebf9a9980a6761802bbae
rajatharya <rajatharya@gmail.com>
2016-09-01 19:11:41 +0530

    Solving the day 25 running time complexity problem

--- src/day25runnintime/Solution.java
+++ src/day25runnintime/Solution.java
@@ -1,19 +1,31 @@
 package day25runnintime;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;;
 
 public class Solution {
 
-    public static void main(String[] args) {
-    	 Scanner scanner = new Scanner(System.in);
-    	 int T = scanner.nextInt();
-    	 while (T-- > 0) {
-			
+	public static void isPrime(int number) {
+		boolean isPrime = true;
+		for (int j = 2; j < Math.sqrt(number); j++) {
+			if (number % j == 0) {
+				isPrime = false;
+			}
 		}
-    }
-}
+		if (number == 1) {
+			System.out.println("Not prime");
+		} else if (isPrime) {
+			System.out.println("Prime");
+		} else {
+			System.out.println("Not prime");
+		}
+	}
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+		while (T-- > 0) {
+			int inputNumber = scanner.nextInt();
+			isPrime(inputNumber);
+		}
+	}
+}
